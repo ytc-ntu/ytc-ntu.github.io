b3dc96b467f81a67574e6b5beb994eeefe5c3760
Kiên Nguyễn Trọng <13795091+trongkien1008@users.noreply.github.com>
2022-05-23 10:58:27 +0700

    Update main.js

--- avatar-frame/js/main.js
+++ avatar-frame/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     var output_name = rand_string(5)+' YTC.png';
     
         var text = { 'frames' : [
-		{'name':'Tháng Thanh Niên NTU', 'src' :'uploads/dtn.png'}, 
+		{'name':'Chào mừng đại Hội XXVII', 'src' :'uploads/avatarframe-02.png'}, 
 		
 	    ]};
 
